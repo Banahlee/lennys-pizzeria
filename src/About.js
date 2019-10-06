523e41e0d38762d1a84066c35f9071e559897d55
Banah Lee <44130702+Banahlee@users.noreply.github.com>
2019-10-06 14:00:40 -0700

    Add files via upload

--- src/About.js
+++ src/About.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import hands from './imgs/hands.jpg';
+import oven from './imgs/oven.jpg';
+import pizza from './imgs/pizza.jpg';
 
 const About = () => (
     <div>
@@ -9,10 +12,22 @@ const About = () => (
             </div>
         </div>
         <div className='about-story'>
-            <p>our story</p>
-        </div>
-        <div className='about-ratings'>
-            <p>ratings</p>
+            <h1>our motto:</h1>
+            <h2>Eat Good and Relax.</h2>
+            <div className='about-story-content '>
+                <div className='s container'>
+                    <p className=''>Founded in 1983, Lenny's has always been about one thing, quality.
+                        Since then we've been the staple "go-to" place in our community a for great atmoshpere, and even better pizzas.
+                    </p>
+                </div>
+            </div>
+            <div className='about-img-list'>
+                <ul>
+                    <li><img src={pizza} height='400px' width='450px'/></li>
+                    <li><img src={hands} height='400px' width='450px'/></li>
+                    <li><img src={oven} height='400px' width='450px'/></li>
+                </ul>
+            </div>
         </div>
     </div>
 )
